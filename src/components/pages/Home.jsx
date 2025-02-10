fdc23fa5878ea3a9e15b1df664211c986c4f4d11
ben <gomben397@hengersor.hu>
2025-02-10 11:27:28 +0100

    asd

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Home = ()=>
+{
+  return (
+    <>
+    {/* <div className='navbar'>
+      <Navbar>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/Contact' element={<Contact />}/>
+          <Route path='/About' element={<About />}/>
+        </Routes>
+      </Navbar>
+    </div> */}
+        <div>
+            <h1>Házikó</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolores debitis laborum!</p>
+        </div>
+    </>
+  )
+}
---
+++ src/components/pages/Services.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Services = ()=>
+{
+  return (
+    <>
+        <div>
+            <h1>Szolgáltatásaink</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolores debitis laborum!</p>
+        </div>
+    </>
+  )
+}
